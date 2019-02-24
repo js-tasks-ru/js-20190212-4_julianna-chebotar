e1510288a1e33ac71352d941651bcdc09c2b25a2
Julianna609 <chebotar609@gmail.com>
2019-02-24 16:47:05 +0200

    solved 2-module, 3-module 1-task, 4-module 1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Генерация HTML списка друзей
  * @param {Friend[]} friends
  * @return {HTMLUListElement}
  */
-function makeFriendsList (friends) {
+function makeFriendsList(friends) {
+  const ul = document.createElement("ul");
+  const render = item => {
+    const li = document.createElement("li");
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    ul.appendChild(li);
+  };
+
+  for (let item of friends) {
+    render(item);
+  }
+  return ul;
 }
