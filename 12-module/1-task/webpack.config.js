dbef17b5dc65dfaf9928f73334ab2ce1a0be83e3
Julianna609 <chebotar609@gmail.com>
2019-04-23 23:44:48 +0300

    solved 12-module

--- 12-module/1-task/webpack.config.js
+++ 12-module/1-task/webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 module.exports = {
     /**
      * Здесь должен быть описан ваш конфиг для сборки.
      * Для ссылки на текущую папку используйте встроенную переменную __dirname
      */
+    entry: './modules/app.js',
+
+    output: {
+      path: path.resolve(__dirname, "./"),
+      filename: 'index.js'
+    },
 };
